b0971d9397b86cd8c1ecaba1de7b39c28a57e572
Glauton Santos <glautoncardoso@gmail.com>
2020-05-30 16:02:48 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const hashtags = [
+	"#restaurante",
+	"#delivery",
+	"#tecnologia",
+	"#startup",
+	"#food",
+	"#café",
+	"#fitnessfood",
+	"#comidasaudável",
+	"#instafood",
+	"#food",
+	"#hamburgueria",
+	"#pizzaria",
+	"#sushi",
+	"#pastelaria",
+	"#cookies",
+	"#massas",
+	"#lasanha",
+	"#macarronada",
+	"#comida",
+	"#salgados",
+	"#agendamento",
+	"#compras",
+	"#mercado",
+	"#frango",
+	"#quentinhas",
+	"#marmita",
+	"#galeto",
+	"#coxinha",
+	"#entregas",
+	"#motoboys",
+	"#ifood",
+	"#ubereats",
+	"#rappi",
+	"#cardapioweb",
+	"#deliverydecomida",
+	"#fooddelivery",
+	"#pizza",
+	"#fiqueemcasa"
+]
+
+function randomElements(items) {
+	var item = items[Math.floor(Math.random() * items.length)];
+	return item;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomIntInclusive(1, 10)
+
+function removeElementFromArray(array, item) {
+	const index = array.indexOf(item);
+	if (index > -1) {
+	  array.splice(index, 1);
+	}
+	return array;
+}
+
+function chooseHashtags(array) {
+	var item = null;
+	var newArray = array;
+	var choosenHashtagsArray = [];
+	for(let i = 0; i < getRandomIntInclusive(4, array.length); i++){
+		item = randomElements(array);
+		newArray = removeElementFromArray(array, item);
+		choosenHashtagsArray.push(item);
+	}
+
+	return choosenHashtagsArray
+}
+
+console.log(chooseHashtags(hashtags).join(' '))
